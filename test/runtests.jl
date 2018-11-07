a9d03169457eb780519398c72f098de27215ceda
konkam <guillaume.konkamking.work@gmail.com>
2018-11-07 13:28:20 +0100

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,7 @@ println("Testing...")
 using Test
 using ExactWrightFisher
 @test 1 == 1
+
+include("test_helper_functions.jl")
+include("test_A_infinity_functions.jl")
+include("test_exact_wright_fisher_functions.jl")
