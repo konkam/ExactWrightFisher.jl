3482c0ba3e7fc4ea960fdaf6d9a9d72eee9004ee
konkam <guillaume.konkamking.work@gmail.com>
2018-11-07 18:57:53 +0100

    some beginning of documentation

--- src/helper_functions.jl
+++ src/helper_functions.jl
@@ -3,8 +3,13 @@
     minus_1_power_i(i::Int64)
 
 Returns \$(-1)^i\$ with hopefully the least amount of work necessary.
-"""
 
+# Examples
+```julia-repl
+julia> minus_1_power_i(1)
+-1
+```
+"""
 function minus_1_power_i(i::Int64)
     if iseven(i)
         return 1
@@ -16,9 +21,22 @@ end
 """
     signed_log_sum_exp(lx, signs)
 
-logsumexp with potentially subtraction of terms
-"""
+logsumexp with potentially subtraction of terms.
 
+...
+# Arguments
+- `lx`: an array of logterms.
+- `signs`: an array of signs for these terms.
+...
+
+# Examples
+```julia-repl
+julia>signed_log_sum_exp([0.2, -3., 1], [1, 1, -1])
+2-element Array{Float64,1}:
+ -1.0
+  0.36955602678296184
+```
+"""
 function signed_log_sum_exp(lx, signs)
   m = maximum(lx)
   scaled_sum = sum(signs .* exp.(lx .- m))
