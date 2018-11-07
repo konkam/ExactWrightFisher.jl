214090f25e3c2e2f10ae95a5c4ed6b3870e7f72b
konkam <guillaume.konkamking.work@gmail.com>
2018-11-07 14:46:48 +0100

    added progress meter

--- src/Wright_Fisher_exact_simulation.jl
+++ src/Wright_Fisher_exact_simulation.jl
@@ -1,3 +1,5 @@
+using ProgressMeter
+
 function Wright_Fisher_exact_transition(x::T, t::T, theta_1::T, theta_2::T) where T<:Real
   if t == 0
     return x
@@ -31,27 +33,30 @@ function Wright_Fisher_K_dim_exact_transition(xvec::AbstractVector{T}, t::Real, 
   end
 end
 
-function cmp_K_dim_trajectory(initial_vec::AbstractVector{T}, times::AbstractVector{T}, transition_function::Function) where T<:Real
+function cmp_K_dim_trajectory(initial_vec::AbstractVector{T}, times::AbstractVector{T}, transition_function::Function; use_progress_meter = false) where T<:Real
   trajectory = Array{Float64}(undef, length(initial_vec), length(times) + 1)
   times_with_0 = [0; times]
   trajectory[:,1] = initial_vec
   imax = length(times)+1
-  # pb = progress_bar$new(total = imax, format = "  [:bar] :percent in :elapsed, eta: :eta")
-  # pb$tick(0)
-  for i in 2:imax
-    # progress(i, progress.bar = TRUE, max.value = imax)
-    # pb$tick()
-    trajectory[:,i] = transition_function(trajectory[:,i-1], times_with_0[i] - times_with_0[i-1])
+
+  if use_progress_meter
+    @showprogress 3 "Computing trajectory..." for i in 2:imax
+      trajectory[:,i] = transition_function(trajectory[:,i-1], times_with_0[i] - times_with_0[i-1])
+    end
+  else
+    for i in 2:imax
+      trajectory[:,i] = transition_function(trajectory[:,i-1], times_with_0[i] - times_with_0[i-1])
+    end
   end
   return trajectory
 end
 
-function Wright_Fisher_K_dim_exact_trajectory(initial_vec::AbstractVector{T}, times::AbstractVector{T}, αvec::AbstractVector{T}) where T<:Real
+function Wright_Fisher_K_dim_exact_trajectory(initial_vec::AbstractVector{T}, times::AbstractVector{T}, αvec::AbstractVector{T}; use_progress_meter = false) where T<:Real
   sα = sum(αvec)
 
   function WF_exact_transition_function(st::AbstractVector{T}, dt::Real)::Vector{Float64}  where T<:Real
     return Wright_Fisher_K_dim_exact_transition(st, dt, αvec, sα)
   end
 
-  return cmp_K_dim_trajectory(initial_vec, times, WF_exact_transition_function)
+  return cmp_K_dim_trajectory(initial_vec, times, WF_exact_transition_function; use_progress_meter = use_progress_meter)
 end
--- test/test_exact_wright_fisher_functions.jl
+++ test/test_exact_wright_fisher_functions.jl
@@ -22,4 +22,5 @@ using Random, Distributions
     for i in 1:length(res[:,end])
         @test res[i, end] ≈ [0.52047555209178, 0.04761111823097684, 1.3036544865733622e-5, 0.4319002931323774][i] atol=10^(-8)
     end
+    @test_nowarn ExactWrightFisher.Wright_Fisher_K_dim_exact_trajectory(rand(Dirichlet(α_vec |> collect)), range(0, stop = 1, length = 10), α_vec; use_progress_meter = true)
 end
