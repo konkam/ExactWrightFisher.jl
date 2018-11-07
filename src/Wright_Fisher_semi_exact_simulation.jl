e75471dae8caad00c117e71dd969b6f1a694ba1e
konkam <guillaume.konkamking.work@gmail.com>
2018-11-07 17:33:02 +0100

    added 1D functions

--- src/Wright_Fisher_semi_exact_simulation.jl
+++ src/Wright_Fisher_semi_exact_simulation.jl
@@ -1,3 +1,25 @@
+function Wright_Fisher_exact_transition_with_t005_approx(x::Real, t::Real, theta_1::Real, theta_2::Real)
+  if t == 0
+    return x
+  elseif t<=0.05
+    A∞ = Compute_A∞_approx(theta_1 + theta_2, t)
+  else
+    A∞ = Compute_A∞(theta_1 + theta_2, t)
+  end
+  L = rand(Binomial(A∞, x))
+  Y = rand(Beta(theta_1 + L, theta_2 + A∞ - L))
+  return Y
+end
+
+function Wright_Fisher_exact_trajectory_with_t005_approx(initial_value::Real, times::AbstractVector{T}, theta_1::Real, theta_2::Real; use_progress_meter = false) where T<:Real
+
+  function  WF_1D_transition_fun(st::Real, dt::Real)::Real
+    return Wright_Fisher_exact_transition_with_t005_approx(st, dt, theta_1, theta_2)
+  end
+
+  return cmp_1D_trajectory(initial_value, times, WF_1D_transition_fun; use_progress_meter = use_progress_meter)
+end
+
 function Wright_Fisher_K_dim_transition_with_t005_approx(xvec::AbstractVector{T}, t::Real, αvec::AbstractVector{T}, sα::Real) where T<:Real
   # sα = sum(αvec)
   if t==0
