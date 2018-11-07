aec0068e959de53fc38ecc6d1e99a53d936dad1f
konkam <guillaume.konkamking.work@gmail.com>
2018-11-07 13:28:40 +0100

    included new files

--- src/ExactWrightFisher.jl
+++ src/ExactWrightFisher.jl
@@ -2,4 +2,11 @@ module ExactWrightFisher
 
 greet() = print("Hello World!")
 
+export minus_1_power_i
+
+include("helper_functions.jl")
+include("A_infinity.jl")
+include("A_infinity_approx.jl")
+include("Wright_Fisher_exact_simulation.jl")
+include("Wright_Fisher_semi_exact_simulation.jl")
 end # module
