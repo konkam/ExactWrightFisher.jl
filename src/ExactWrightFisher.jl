7950c8b3b42c5a013854716b7bdd23672a5af989
konkam <guillaume.konkamking.work@gmail.com>
2018-11-08 11:19:14 +0100

    removed automatic line from package creation

--- src/ExactWrightFisher.jl
+++ src/ExactWrightFisher.jl
@@ -1,7 +1,5 @@
 module ExactWrightFisher
 
-greet() = print("Hello World!")
-
 export Wright_Fisher_exact_transition, Wright_Fisher_exact_trajectory, Wright_Fisher_exact_transition_with_t005_approx, Wright_Fisher_exact_trajectory_with_t005_approx, Wright_Fisher_K_dim_exact_transition, Wright_Fisher_K_dim_exact_trajectory, Wright_Fisher_K_dim_transition_with_t005_approx, Wright_Fisher_K_dim_trajectory_with_t005_approx
 
 include("helper_functions.jl")
