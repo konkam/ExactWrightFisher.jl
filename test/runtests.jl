e0c0663e57f4d43fac9f012061228baec616612f
konkam <guillaume.konkamking.work@gmail.com>
2018-11-12 15:59:20 +0100

    added a measure of test duration

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 using ExactWrightFisher
 @test 1 == 1
 
-include("test_helper_functions.jl")
-include("test_A_infinity_functions.jl")
-include("test_exact_wright_fisher_functions.jl")
-include("test_semi_exact_wright_fisher_functions.jl")
+@time include("test_helper_functions.jl")
+@time include("test_A_infinity_functions.jl")
+@time include("test_exact_wright_fisher_functions.jl")
+@time include("test_semi_exact_wright_fisher_functions.jl")
