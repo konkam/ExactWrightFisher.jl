62b49dcc368f67a08322dd64f55d84b8914ce6a4
konkam <guillaume.konkamking.work@gmail.com>
2018-12-13 14:25:22 +0100

    with with large precision

--- test/test_helper_functions.jl
+++ test/test_helper_functions.jl
@@ -6,7 +6,7 @@ using StatsFuns
     res = ExactWrightFisher.signed_logsumexp(log.(1:5), repeat([1],5))
     @test res[1] == 1
     @test logsumexp(log.(1:5)) == res[2]
-    
+
     res = ExactWrightFisher.signed_logsumexp([2, 4, 2], [1, -1, 1])
     @test res[1] == -1.0
 
@@ -17,4 +17,7 @@ using StatsFuns
     res = ExactWrightFisher.signed_logsumexp([log(3), log(3), log(4), log(4)], [1, -1, 1, -1])
     @test res[1] == 1.0
     @test res[2] == -Inf
+
+    ref = ExactWrightFisher.signed_logsumexp(BigFloat[0.2, -3., 1], BigFloat[1, 1, -1])
+    @test ExactWrightFisher.signed_logsumexp([0.2, -3., 1], [1, 1, -1])[2] ≈ Float64(ref[2])
 end
