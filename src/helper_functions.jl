3475ac13ad79bf4c5905c697246c8ff58cfc9469
konkam <guillaume.konkamking.work@gmail.com>
2018-12-13 14:13:52 +0100

    probably better logsumexp

--- src/helper_functions.jl
+++ src/helper_functions.jl
@@ -1,4 +1,4 @@
-
+using StatsFuns
 """
     minus_1_power_i(i::Int64)
 
@@ -37,16 +37,41 @@ julia>signed_logsumexp([0.2, -3., 1], [1, 1, -1])
   0.36955602678296184
 ```
 """
+# function signed_logsumexp(lx, signs)
+#   m = maximum(lx)
+#   scaled_sum = sum(signs .* exp.(lx .- m))
+#   if abs(scaled_sum) <= 10*eps(Float64)
+#     return [1., -Inf]
+#   elseif scaled_sum < 0
+#     sgn = -1
+#     scaled_sum = -1*scaled_sum
+#   else
+#     sgn = 1
+#   end
+#   return [sgn, m + log(scaled_sum) ]#Will give an error if the sum is negative
+# end
+
 function signed_logsumexp(lx, signs)
-  m = maximum(lx)
-  scaled_sum = sum(signs .* exp.(lx .- m))
-  if abs(scaled_sum) <= 10*eps(Float64)
-    return [1., -Inf]
-  elseif scaled_sum < 0
-    sgn = -1
-    scaled_sum = -1*scaled_sum
+  # @assert length(lx) == length(signs)
+  # pos = signs .== 1
+  if all(signs .> 0)
+    return [1.0, logsumexp(lx)]
+  elseif all(signs .< 0)
+    return [-1.0, logsumexp(lx)]
   else
-    sgn = 1
+    n = length(lx)
+    @inbounds logsumexp_positive_terms = logsumexp(lx[i] for i in 1:n if signs[i] > 0)
+    @inbounds logsumexp_negative_terms = logsumexp(lx[i] for i in 1:n if signs[i] < 0)
+    if logsumexp_positive_terms > logsumexp_negative_terms
+      sgn = 1
+      res = log(1-exp(logsumexp_negative_terms - logsumexp_positive_terms)) + logsumexp_positive_terms
+    elseif logsumexp_positive_terms < logsumexp_negative_terms
+      sgn = -1
+      res = log(1-exp(logsumexp_positive_terms - logsumexp_negative_terms)) + logsumexp_negative_terms
+    else
+      sgn = 1
+      res = -Inf
+    end
+    return [sgn, res]
   end
-  return [sgn, m + log(scaled_sum) ]#Will give an error if the sum is negative
 end
