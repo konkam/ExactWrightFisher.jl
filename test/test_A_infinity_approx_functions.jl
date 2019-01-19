c720e3b458b86547859822a88eac4beec201b274
konkam <guillaume.konkamking.work@gmail.com>
2019-01-19 13:03:22 +0100

    testfile was not included in runtests

--- test/test_A_infinity_approx_functions.jl
+++ test/test_A_infinity_approx_functions.jl
@@ -2,7 +2,7 @@ using Random
 
 @testset "testing approximate Ancestor(A∞) functions" begin
     @test ExactWrightFisher.σ(1., 0.05) ≈ 0.6493907790932706 atol=10^(-8)
-    @test ExactWrightFisher.σ(1., 1) ≈ 2./3
+    @test ExactWrightFisher.σ(1., 1; silence = false) ≈ 2. / 3
     Random.seed!(0)
     @test ExactWrightFisher.Compute_A∞_approx(sum(1:4), 0.05) == 45
 end
