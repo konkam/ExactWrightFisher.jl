21226cdc115831596f881cb35aa45783cb01587f
konkam <guillaume.konkamking.work@gmail.com>
2026-05-31 01:43:19 +0200

    updated test

--- test/test_A_infinity_approx_functions.jl
+++ test/test_A_infinity_approx_functions.jl
@@ -1,8 +1,8 @@
 using Random
 
 @testset "testing approximate Ancestor(A∞) functions" begin
-    @test ExactWrightFisher.σ(1., 0.05) ≈ 0.6493907790932706 atol=10^(-8)
-    @test ExactWrightFisher.σ(1., 1) ≈ 2. / 3
+    @test ExactWrightFisher.σ2(1., 0.05) ≈ 0.6493907790932706 atol=10^(-8)
+    @test ExactWrightFisher.σ2(1., 1) ≈ 2. / 3
     Random.seed!(0)
     @test_nowarn ExactWrightFisher.Compute_A∞_approx(sum(1:4), 0.05)
 end
